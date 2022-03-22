d6c3dd97a6d712208a9f1b473be0df07154eb49c
cesarbahena <cabahenac@gmail.com>
2022-03-22 10:05:12 +0000

    feat(project): establish project structure, add tests for pagination

--- test-invalid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Setting up invalid date test..."
-sed -i 's/01\/02\/2023 11:00:00 AM/INVALID_DATE/' consulta.html
-echo "Running invalid data test..."
-lims-scraper
-echo "Restoring valid data..."
-sed -i 's/INVALID_DATE/01\/02\/2023 11:00:00 AM/' consulta.html
-echo "Test completed, HTML restored."
--- test-valid.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Running valid data test..."
-lims-scraper
